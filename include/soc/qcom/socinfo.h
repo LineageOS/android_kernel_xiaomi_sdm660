f92aa258e7744ebce213e6c7d831457191c937d3
Srinivas Ramana <sramana@codeaurora.org>
2017-08-18 20:57:51 +0530

    soc: qcom: socinfo: Add support for SDM636 SOC
    Add socinfo support and CPU ID for SDM636 SOC.
    
    Signed-off-by: Srinivas Ramana <sramana@codeaurora.org>

--- include/soc/qcom/socinfo.h
+++ include/soc/qcom/socinfo.h
@@ -92,6 +92,8 @@
 	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,sdmmagpie")
 #define early_machine_is_sdm660()	\
 	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,sdm660")
+#define early_machine_is_sdm636()	\
+	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,sdm636")
 #define early_machine_is_bengal_iot()	\
 	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,bengal-iot")
 #define early_machine_is_bengalp_iot()	\
@@ -149,6 +151,7 @@
 #define early_machine_is_sdxprairie()	0
 #define early_machine_is_sdmmagpie()	0
 #define early_machine_is_sdm660()	0
+#define early_machine_is_sdm636()	0
 #define early_machine_is_bengal_iot()	0
 #define early_machine_is_bengalp_iot()	0
 #define early_machine_is_msm8937()	0
@@ -179,6 +182,7 @@ enum msm_cpu {
 	MSM_CPU_8084,
 	MSM_CPU_8996,
 	MSM_CPU_SDM660,
+	MSM_CPU_SDM636,
 	MSM_CPU_SM8150,
 	MSM_CPU_SA8150,
 	MSM_CPU_KONA,
