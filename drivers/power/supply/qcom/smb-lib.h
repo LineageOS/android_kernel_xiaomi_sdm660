cdd937af07ce20f0b64a645f1c5ad801afade4e8
Akhil Narang <akhilnarang.1999@gmail.com>
2018-07-09 17:10:49 +0530

    Merge remote-tracking branch 'caf/kernel.lnx.4.4.r27-rel' into lineage-15.1
    * caf/kernel.lnx.4.4.r27-rel: (87 commits)
      ANDROID: sdcardfs: Use lower getattr times/size
      msm: ipa: fix to not allow NAT DMA command without device initialization
      diag: Validate query dci event and log mask size properly
      msm: ipa: fix to not allow NAT DMA command without device initialization
      rtc: Disable alarm irq if alarm time is in the past
      ipv6: fix udpv6 sendmsg crash caused by too small MTU
      ARM: dts: qcom: Remove lpm performance index for sdm660
      Revert "ARM: dts: msm: Enable ACD functionality for SDM660 silver cluster"
      clk: qcom: Retrieve pre_div from freq_tbl for shared RCG
      ipv6: ip6_make_skb() needs to clear cork.base.dst
      ipv6: fix possible mem leaks in ipv6_make_skb()
      ANDROID: Bluetooth: hidp: buffer overflow in hidp_process_report
      blkcg: fix double free of new_blkg in blkcg_init_queue
      KEYS: fix out-of-bounds read during ASN.1 parsing
      ASN.1: fix out-of-bounds read when parsing indefinite length item
      ARM: dts: msm: Add support for truly FHD panel on SDM660 CDP
      ARM: dts: msm: Add support for new speed bin for SDM660
      ARM: dts: msm: Add speed-bin 4 support for sdm660
      msm: kgsl: Don't dump GPMU registers on non GPMU devices
      msm: kgsl: Don't dump GPMU registers on non GPMU devices
      ...
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>
    
     Conflicts:
    	arch/arm/boot/dts/qcom/Makefile
    	drivers/leds/leds-qpnp-flash-v2.c
    	drivers/regulator/cprh-kbss-regulator.c
    	sound/soc/codecs/sdm660_cdc/msm-analog-cdc.c
    
    Change-Id: I72e43b2abbb69d5e44b7bcea7328d06dd058b10f

--- drivers/power/supply/qcom/smb-lib.h
+++ drivers/power/supply/qcom/smb-lib.h
@@ -66,6 +66,7 @@ enum print_reason {
 #define OTG_DELAY_VOTER			"OTG_DELAY_VOTER"
 #define USBIN_I_VOTER			"USBIN_I_VOTER"
 #define WEAK_CHARGER_VOTER		"WEAK_CHARGER_VOTER"
+#define OV_VOTER			"OV_VOTER"
 
 #define CONFIG_CHARGER_RUNIN
 #define THERMAL_CONFIG_FB 1
@@ -87,6 +88,7 @@ enum {
 	TYPEC_CC2_REMOVAL_WA_BIT	= BIT(2),
 	QC_AUTH_INTERRUPT_WA_BIT	= BIT(3),
 	OTG_WA				= BIT(4),
+	OV_IRQ_WA_BIT			= BIT(5),
 };
 
 enum smb_irq_index {
