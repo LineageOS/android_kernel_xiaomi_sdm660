4f93e862d4a39b7b98647f8905e9da37c6b83d1e
dianlujitao <dianlujitao@lineageos.org>
2018-09-22 20:45:05 +0800

    soc: qcom: sleepstate: Import Xiaomi MI changes
    Change-Id: I7063375025c92a928407be797e3d95e70cde5f46

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -4,6 +4,7 @@
  * Copyright (c) 2003 Patrick Mochel
  * Copyright (c) 2003 Open Source Development Lab
  * Copyright (c) 2009 Rafael J. Wysocki <rjw@sisk.pl>, Novell Inc.
+ * Copyright (C) 2019 XiaoMi, Inc.
  *
  * This file is released under the GPLv2.
  */
@@ -32,11 +33,14 @@
 #include <linux/moduleparam.h>
 #include <linux/wakeup_reason.h>
 
+#include <linux/gpio.h>
 #include "power.h"
 
 const char *pm_labels[] = { "mem", "standby", "freeze", NULL };
 const char *pm_states[PM_SUSPEND_MAX];
 
+extern int PROC_AWAKE_ID; /* 12th bit */
+extern int slst_gpio_base_id;
 unsigned int pm_suspend_global_flags;
 EXPORT_SYMBOL_GPL(pm_suspend_global_flags);
 
@@ -563,7 +567,11 @@ int pm_suspend(suspend_state_t state)
 		return -EINVAL;
 
 	pm_suspend_marker("entry");
+	gpio_set_value (slst_gpio_base_id+PROC_AWAKE_ID, 0);
+	pr_debug("%s: PM_SUSPEND_PREPARE %d \n", __func__, slst_gpio_base_id + PROC_AWAKE_ID);
 	error = enter_state(state);
+	gpio_set_value (slst_gpio_base_id+PROC_AWAKE_ID, 1);
+	pr_debug("%s: PM_POST_SUSPEND %d \n", __func__, slst_gpio_base_id + PROC_AWAKE_ID);
 	if (error) {
 		suspend_stats.fail++;
 		dpm_save_failed_errno(error);
