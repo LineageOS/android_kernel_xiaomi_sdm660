effbe8308bbe7cc8c337b8840f1eac68af59f7d8
Amar Singhal <asinghal@codeaurora.org>
2019-08-07 12:36:27 -0700

    qcacmn: Remove default pdev id from regulatory component
    Default pdev id is redundant now. Therefore remove def_pdev_id from
    the regulatory component.
    
    Change-Id: I383f904e373df0e7d02fb0e2d00141fcacebe82a
    CRs-Fixed: 2505161

--- umac/regulatory/core/src/reg_offload_11d_scan.c
+++ umac/regulatory/core/src/reg_offload_11d_scan.c
@@ -381,6 +381,7 @@ QDF_STATUS reg_save_new_11d_country(struct wlan_objmgr_psoc *psoc,
 	struct wlan_lmac_if_reg_tx_ops *tx_ops;
 	struct set_country country_code;
 	uint8_t pdev_id;
+	uint8_t ctr;
 
 	psoc_priv_obj = reg_get_psoc_obj(psoc);
 	if (!psoc_priv_obj) {
@@ -389,8 +390,14 @@ QDF_STATUS reg_save_new_11d_country(struct wlan_objmgr_psoc *psoc,
 		return QDF_STATUS_E_FAILURE;
 	}
 
-	pdev_id = psoc_priv_obj->def_pdev_id;
-	psoc_priv_obj->new_11d_ctry_pending[pdev_id] = true;
+	/*
+	 * Need firmware to send channel list event
+	 * for all phys. Therefore set pdev_id to 0xFF
+	 */
+	pdev_id = 0xFF;
+	for (ctr = 0; ctr < psoc_priv_obj->num_phy; ctr++)
+		psoc_priv_obj->new_11d_ctry_pending[ctr] = true;
+
 	qdf_mem_copy(country_code.country, country, REG_ALPHA2_LEN + 1);
 	country_code.pdev_id = pdev_id;
 
@@ -399,8 +406,10 @@ QDF_STATUS reg_save_new_11d_country(struct wlan_objmgr_psoc *psoc,
 		if (tx_ops->set_country_code) {
 			tx_ops->set_country_code(psoc, &country_code);
 		} else {
-			reg_err("NULL country set handler");
-			psoc_priv_obj->new_11d_ctry_pending[pdev_id] = false;
+			reg_err("country set handler is not present");
+			for (ctr = 0; ctr < psoc_priv_obj->num_phy; ctr++)
+				psoc_priv_obj->new_11d_ctry_pending[ctr] =
+					false;
 			return QDF_STATUS_E_FAULT;
 		}
 	}
