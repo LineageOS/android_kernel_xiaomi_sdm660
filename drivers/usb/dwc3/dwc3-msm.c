4251235731fc7c9c933c0329486293bbf529c0cd
Davide Garberi <dade.garberi@gmail.com>
2019-08-21 14:54:16 +0200

    usb: Import xiaomi longcheer changes
    Change-Id: Ifcfd9b5fce256553f1356b074ae582658646d599
    Signed-off-by: Isaac Chen <tingyi364@gmail.com>

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -55,7 +55,11 @@
 #include "debug.h"
 #include "xhci.h"
 
+#ifdef CONFIG_MACH_LONGCHEER
+#define SDP_CONNETION_CHECK_TIME 5000 /* in ms */
+#else
 #define SDP_CONNETION_CHECK_TIME 10000 /* in ms */
+#endif
 
 /* time out to wait for USB cable status notification (in ms)*/
 #define SM_INIT_TIMEOUT 30000
@@ -2734,6 +2738,10 @@ static void check_for_sdp_connection(struct work_struct *w)
 	struct dwc3_msm *mdwc =
 		container_of(w, struct dwc3_msm, sdp_check.work);
 	struct dwc3 *dwc = platform_get_drvdata(mdwc->dwc3);
+#ifdef CONFIG_MACH_LONGCHEER
+	union power_supply_propval pval = {0};
+	int ret;
+#endif
 
 	if (!mdwc->vbus_active)
 		return;
@@ -2749,6 +2757,17 @@ static void check_for_sdp_connection(struct work_struct *w)
 	if (dwc->gadget.state < USB_STATE_DEFAULT &&
 		dwc3_gadget_get_link_state(dwc) != DWC3_LINK_STATE_CMPLY) {
 		mdwc->vbus_active = 0;
+#ifdef CONFIG_MACH_LONGCHEER
+		if (!mdwc->usb_psy)
+			mdwc->usb_psy = power_supply_get_by_name("usb");
+		if (mdwc->usb_psy) {
+			pval.intval = 1;
+			ret = power_supply_set_property(mdwc->usb_psy,
+					POWER_SUPPLY_PROP_RERUN_APSD, &pval);
+			if (ret)
+				dev_dbg(mdwc->dev, "error when set property\n");
+		}
+#endif
 		dbg_event(0xFF, "Q RW SPD CHK", mdwc->vbus_active);
 		queue_work(mdwc->dwc3_wq, &mdwc->resume_work);
 	}
