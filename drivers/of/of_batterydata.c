4f74eff23f1a292653058837969e2e9ba2ef42f6
Davide Garberi <dade.garberi@gmail.com>
2019-08-28 22:02:39 +0200

    of_batterydata: Import xiaomi edits
    * Slightly fixed code styling
    
    Change-Id: Ia6f42fb1f015bf4446bed08c5da294c26a8a8795
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>
    Signed-off-by: Isaac Chen <tingyi364@gmail.com>

--- drivers/of/of_batterydata.c
+++ drivers/of/of_batterydata.c
@@ -338,6 +338,7 @@ struct device_node *of_batterydata_get_best_profile(
 	 * Find the battery data with a battery id resistor closest to this one
 	 */
 	for_each_child_of_node(batterydata_container_node, node) {
+#ifndef CONFIG_MACH_LONGCHEER
 		if (batt_type != NULL) {
 			rc = of_property_read_string(node, "qcom,battery-type",
 							&battery_type);
@@ -347,6 +348,7 @@ struct device_node *of_batterydata_get_best_profile(
 				break;
 			}
 		} else {
+#endif
 			rc = of_batterydata_read_batt_id_kohm(node,
 							"qcom,batt-id-kohm",
 							&batt_ids);
@@ -368,11 +370,25 @@ struct device_node *of_batterydata_get_best_profile(
 					best_id_kohm = batt_ids.kohm[i];
 				}
 			}
+#ifndef CONFIG_MACH_LONGCHEER
 		}
+#endif
 	}
 
 	if (best_node == NULL) {
+#ifdef CONFIG_MACH_LONGCHEER
+		for_each_child_of_node(batterydata_container_node, node) {
+			rc = of_property_read_string(node, "qcom,battery-type", &battery_type);
+			if (!rc && strcmp(battery_type,"unknown-default") == 0) {
+				best_node = node;
+				break;
+			}
+		}
+		if(best_node)
+			pr_info("Use unknown battery data\n");
+#else
 		pr_err("No battery data found\n");
+#endif
 		return best_node;
 	}
 
