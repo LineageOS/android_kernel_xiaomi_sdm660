8ede0ffdbdea1306cecae2f57b896759fc74fe56
dianlujitao <dianlujitao@lineageos.org>
2020-02-07 15:45:33 +0800

    msm: thermal: Import Xiaomi changes
    Change-Id: If93547597f662f0cf0ee462e0fb674f0081073fa

--- drivers/thermal/thermal_core.h
+++ drivers/thermal/thermal_core.h
@@ -2,6 +2,7 @@
  *  thermal_core.h
  *
  *  Copyright (C) 2012  Intel Corp
+ *  Copyright (C) 2019 XiaoMi, Inc.
  *  Author: Durgadoss R <durgadoss.r@intel.com>
  *
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -54,6 +55,17 @@ struct thermal_instance {
 	unsigned int weight; /* The weight of the cooling device */
 };
 
+#ifdef CONFIG_MACH_MI
+struct thermal_message {
+	bool message_ok;
+	const char *batt_array_size;
+	const char *batt_level_screen_on;
+	const char *batt_level_screen_off;
+};
+
+extern struct thermal_message *tm;
+#endif
+
 int thermal_register_governor(struct thermal_governor *);
 void thermal_unregister_governor(struct thermal_governor *);
 
@@ -105,6 +117,10 @@ int of_thermal_get_ntrips(struct thermal_zone_device *);
 bool of_thermal_is_trip_valid(struct thermal_zone_device *, int);
 const struct thermal_trip *
 of_thermal_get_trip_points(struct thermal_zone_device *);
+#ifdef CONFIG_MACH_MI
+int of_parse_thermal_message(void);
+void free_thermal_message(void);
+#endif
 #else
 static inline int of_parse_thermal_zones(void) { return 0; }
 static inline void of_thermal_destroy_zones(void) { }
