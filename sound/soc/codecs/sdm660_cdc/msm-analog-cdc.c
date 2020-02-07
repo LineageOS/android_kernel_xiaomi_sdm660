e47a98b86eb5d42636616f062782aea5c2e97a2f
dianlujitao <dianlujitao@lineageos.org>
2020-02-07 16:07:03 +0800

    ASoC: Import Xiaomi changes
    Change-Id: Iaeb10efd5990b0f2321c1ca64caa60def1a76ea4

--- sound/soc/codecs/sdm660_cdc/msm-analog-cdc.c
+++ sound/soc/codecs/sdm660_cdc/msm-analog-cdc.c
@@ -1,4 +1,5 @@
 /* Copyright (c) 2015-2019, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2019 XiaoMi, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -93,6 +94,9 @@ static const DECLARE_TLV_DB_SCALE(analog_gain, 0, 25, 1);
 static struct snd_soc_dai_driver msm_anlg_cdc_i2s_dai[];
 /* By default enable the internal speaker boost */
 static bool spkr_boost_en = true;
+#ifdef CONFIG_MACH_MI
+bool hs_record_active;
+#endif
 
 static char on_demand_supply_name[][MAX_ON_DEMAND_SUPPLY_NAME_LENGTH] = {
 	"cdc-vdd-mic-bias",
@@ -2584,6 +2588,10 @@ static int msm_anlg_cdc_codec_enable_micbias(struct snd_soc_dapm_widget *w,
 
 		break;
 	case SND_SOC_DAPM_POST_PMU:
+#ifdef CONFIG_MACH_MI
+		if (strnstr(w->name, external2_text, strlen(w->name)))
+			hs_record_active = true;
+#endif
 		if (get_codec_version(sdm660_cdc) <= TOMBAK_2_0)
 			/*
 			 * Wait for 20ms post micbias enable
@@ -2604,6 +2612,12 @@ static int msm_anlg_cdc_codec_enable_micbias(struct snd_soc_dapm_widget *w,
 		}
 		break;
 	case SND_SOC_DAPM_POST_PMD:
+#ifdef CONFIG_MACH_MI
+		snd_soc_update_bits(codec,
+			MSM89XX_PMIC_ANALOG_MICB_1_EN, 0x80, 0x00);
+		if (strnstr(w->name, external2_text, strlen(w->name)))
+			hs_record_active = false;
+#endif
 		if (strnstr(w->name, internal1_text, strlen(w->name))) {
 			snd_soc_update_bits(codec, micb_int_reg, 0xC0, 0x40);
 		} else if (strnstr(w->name, internal2_text, strlen(w->name))) {
