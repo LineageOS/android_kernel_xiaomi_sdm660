adac834beb3860ad0579011645675d26f97b290c
dianlujitao <dianlujitao@lineageos.org>
2020-02-08 14:40:08 +0800

    msm: camera: Import Xiaomi changes
    Change-Id: I2eaec68e6dd23a2dd3329a9ddce23be794fa2fde

--- include/uapi/media/msm_camsensor_sdk.h
+++ include/uapi/media/msm_camsensor_sdk.h
@@ -176,6 +176,9 @@ enum msm_actuator_write_type {
 enum msm_actuator_i2c_operation {
 	MSM_ACT_WRITE = 0,
 	MSM_ACT_POLL,
+#ifdef CONFIG_MACH_MI
+	MSM_ACT_POLL_RESULT,
+#endif
 };
 
 enum actuator_type {
@@ -300,7 +303,8 @@ struct msm_sensor_id_info_t {
 	unsigned short sensor_id;
 	unsigned short sensor_id_mask;
 };
-#ifdef CONFIG_MACH_LONGCHEER
+
+#ifdef CONFIG_MACH_XIAOMI_SDM660
 enum msm_cci_i2c_master_t {
 	MSM_MASTER_0,
 	MSM_MASTER_1,
@@ -325,6 +329,7 @@ struct msm_vcm_id_info_t {
 	enum msm_cci_i2c_master_t cci_i2c_master;
 };
 #endif
+
 struct msm_camera_sensor_slave_info {
 	char sensor_name[32];
 	char eeprom_name[32];
@@ -336,7 +341,7 @@ struct msm_camera_sensor_slave_info {
 	enum i2c_freq_mode_t i2c_freq_mode;
 	enum msm_camera_i2c_reg_addr_type addr_type;
 	struct msm_sensor_id_info_t sensor_id_info;
-#ifdef CONFIG_MACH_LONGCHEER
+#ifdef CONFIG_MACH_XIAOMI_SDM660
 	struct msm_vendor_id_info_t vendor_id_info;
 	struct msm_vcm_id_info_t vcm_id_info;
 #endif
