43a17cbf74fa5756a3c41e27f745232003d8ad92
Davide Garberi <dade.garberi@gmail.com>
2020-02-24 21:44:50 +0100

    input: focal_ft8719: Implement double_tap node
    Change-Id: Ic3e61f975becc9a71d645d62a90d1243f218c940

--- drivers/input/touchscreen/focal_ft8719/focaltech_gesture.c
+++ drivers/input/touchscreen/focal_ft8719/focaltech_gesture.c
@@ -34,6 +34,10 @@
 * 1.Included header files
 *****************************************************************************/
 #include "focaltech_core.h"
+#ifdef CONFIG_TOUCHSCREEN_COMMON
+#include <linux/input/tp_common.h>
+#endif
+
 #if FTS_GESTURE_EN
 /******************************************************************************
 * Private constant and macro definitions using #define
@@ -214,6 +218,33 @@ static ssize_t fts_gesture_buf_store(struct device *dev, struct device_attribute
     /* place holder for future use */
     return -EPERM;
 }
+
+#ifdef CONFIG_TOUCHSCREEN_COMMON
+static ssize_t double_tap_show(struct kobject *kobj,
+                              struct kobj_attribute *attr, char *buf)
+{
+       return sprintf(buf, "%d\n", fts_gesture_data.mode);
+}
+
+static ssize_t double_tap_store(struct kobject *kobj,
+                               struct kobj_attribute *attr, const char *buf,
+                               size_t count)
+{
+       int rc, val;
+
+       rc = kstrtoint(buf, 10, &val);
+       if (rc)
+               return -EINVAL;
+
+       fts_gesture_data.mode = !!val;
+       return count;
+}
+
+static struct tp_common_ops double_tap_ops = {
+       .show = double_tap_show,
+       .store = double_tap_store
+};
+#endif
 
 /*****************************************************************************
 *   Name: fts_create_gesture_sysfs
@@ -536,6 +567,9 @@ int fts_gesture_init(struct fts_ts_data *ts_data)
 {
     struct i2c_client *client = ts_data->client;
     struct input_dev *input_dev = ts_data->input_dev;
+#ifdef CONFIG_TOUCHSCREEN_COMMON
+    int ret;
+#endif
 
     FTS_FUNC_ENTER();
     input_set_capability(input_dev, EV_KEY, KEY_WAKEUP);
@@ -570,6 +604,15 @@ int fts_gesture_init(struct fts_ts_data *ts_data)
     __set_bit(KEY_GESTURE_Z, input_dev->keybit);
 
     fts_create_gesture_sysfs(client);
+
+#ifdef CONFIG_TOUCHSCREEN_COMMON
+       ret = tp_common_set_double_tap_ops(&double_tap_ops);
+       if (ret < 0) {
+               FTS_ERROR("%s: Failed to create double_tap node err=%d\n",
+                       __func__, ret);
+       }
+#endif
+
     fts_gesture_data.mode = ENABLE;
     fts_gesture_data.active = DISABLE;
 
