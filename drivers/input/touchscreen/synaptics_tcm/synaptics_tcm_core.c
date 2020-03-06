b5b58c6603ce89e5a8c181d96e36f6d750774ebf
Erfan Abdi <erfangplus@gmail.com>
2020-03-07 03:18:08 +0330

    input: synaptics_tcm: Enable DT2W node using tp_common
    Change-Id: I9525abed5c6dc24525a3eb6f030a537a66f3ceaf

--- drivers/input/touchscreen/synaptics_tcm/synaptics_tcm_core.c
+++ drivers/input/touchscreen/synaptics_tcm/synaptics_tcm_core.c
@@ -34,6 +34,9 @@
 #include <linux/kthread.h>
 #include <linux/interrupt.h>
 #include "synaptics_tcm_core.h"
+#ifdef CONFIG_TOUCHSCREEN_COMMON
+#include <linux/input/tp_common.h>
+#endif
 
 /* add check F7A LCM by wanghan start */
 extern char g_lcd_id[128];
@@ -398,6 +401,33 @@ exit:
 	LOG_DONE();
 	return retval;
 }
+
+#ifdef CONFIG_TOUCHSCREEN_COMMON
+static ssize_t double_tap_show(struct kobject *kobj,
+                   struct kobj_attribute *attr, char *buf)
+{
+    return sprintf(buf, "%d\n", synaptics_gesture_enable_flag);
+}
+
+static ssize_t double_tap_store(struct kobject *kobj,
+                struct kobj_attribute *attr, const char *buf,
+                size_t count)
+{
+    int rc, val;
+    
+    rc = kstrtoint(buf, 10, &val);
+    if (rc)
+    return -EINVAL;
+    
+    synaptics_gesture_enable_flag = !!val;
+    return count;
+}
+
+static struct tp_common_ops double_tap_ops = {
+    .show = double_tap_show,
+    .store = double_tap_store
+};
+#endif
 
 static ssize_t syna_tcm_sysfs_reset_store(struct device *dev,
 		struct device_attribute *attr, const char *buf, size_t count)
@@ -3326,6 +3356,15 @@ static int syna_tcm_probe(struct platform_device *pdev)
 				"Failed to configure GPIO's\n");
 		goto err_config_gpio;
 	}
+    
+#ifdef CONFIG_TOUCHSCREEN_COMMON
+    retval = tp_common_set_double_tap_ops(&double_tap_ops);
+    if (retval < 0) {
+        dev_err(&pdev->dev,
+                "%s: Failed to create double_tap node err=%d\n",
+                __func__, retval);
+    }
+#endif
 
 	LOGV("create sysfs directory\n");
 	sysfs_dir = kobject_create_and_add(PLATFORM_DRIVER_NAME,
