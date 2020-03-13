f0370c9f4aabb5287cc1c0e26fae88a0800e4e78
Kiran Kumar Lokere <klokere@codeaurora.org>
2020-03-13 16:24:23 -0700

    qcacld-3.0: Set WEP in addba resp from addba req for NAN
    PMF capability information is not available in NAN peer station
    data structure as host does not get full capability information
    of NAN peer hence WEP bit is not set in addba response frame for
    received addba request frames with WEP set.
    
    Set the WEP bit in addba response frame if the received addba request
    frame has WEP bit and it is from NAN peer station.
    
    Change-Id: I9f0a02492b3087d0fd53a7bdde273601913ee2ca
    CRs-Fixed: 2638789

--- core/mac/src/pe/lim/lim_process_action_frame.c
+++ core/mac/src/pe/lim/lim_process_action_frame.c
@@ -1669,6 +1669,11 @@ static void lim_process_addba_req(struct mac_context *mac_ctx, uint8_t *rx_pkt_i
 		pe_warn("warning: unpack addba Req(0x%08x, %d bytes)",
 			status, frame_len);
 	}
+	pe_debug("token %d tid %d timeout %d buff_size %d ssn %d",
+		 addba_req->DialogToken.token, addba_req->addba_param_set.tid,
+		 addba_req->ba_timeout.timeout,
+		 addba_req->addba_param_set.buff_size,
+		 addba_req->ba_start_seq_ctrl.ssn);
 
 	peer = cdp_peer_get_ref_by_addr(soc, pdev, mac_hdr->sa, &peer_id,
 					PEER_DEBUG_ID_WMA_ADDBA_REQ);
@@ -1690,7 +1695,8 @@ static void lim_process_addba_req(struct mac_context *mac_ctx, uint8_t *rx_pkt_i
 			addba_req->addba_param_set.tid,
 			session,
 			addba_req->addba_extn_element.present,
-			addba_req->addba_param_set.amsdu_supp);
+			addba_req->addba_param_set.amsdu_supp,
+			mac_hdr->fc.wep);
 		if (qdf_status != QDF_STATUS_SUCCESS) {
 			pe_err("Failed to send addba response frame");
 			cdp_addba_resp_tx_completion(soc, peer,
