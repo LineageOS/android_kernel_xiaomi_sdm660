7fea1a0f7cc7faddf37cd6a4952ea69a5c654570
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2020-04-06 20:57:26 +0530

    input: touchscreen: nvt_touch_*: Disable proc support
    Change-Id: I14e10f6688e739fb6d206de8b729947ae48afe91

--- drivers/input/touchscreen/nvt_touch_d2t/nt36xxx.h
+++ drivers/input/touchscreen/nvt_touch_d2t/nt36xxx.h
@@ -72,8 +72,8 @@ extern const uint16_t touch_key_array[TOUCH_KEY_NUM];
 #define TOUCH_FORCE_NUM 1000
 
 /*---Customerized func.---*/
-#define NVT_TOUCH_PROC 1
-#define NVT_TOUCH_EXT_PROC 1
+#define NVT_TOUCH_PROC 0
+#define NVT_TOUCH_EXT_PROC 0
 #define NVT_TOUCH_MP 0
 #define MT_PROTOCOL_B 1
 #define WAKEUP_GESTURE 1
