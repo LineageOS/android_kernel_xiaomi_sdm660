bef562810e394fc3de9b3a8294014e2231a268c3
Aditya Kodukula <akodukul@codeaurora.org>
2020-04-15 13:30:14 -0700

    qcacld-3.0: Add a feature flag WLAN_POWER_DEBUG for power stats
    Power stats is moved from debugfs to sysfs, and hence add the feature
    flag WLAN_POWER_DEBUG such that this feature can be compiled out for
    low memory foot print drivers.
    
    Change-Id: Ic395f393580fcd59faa4e4676f67a39ff37bdd1a
    CRs-Fixed: 2665089

--- core/wma/src/wma_main.c
+++ core/wma/src/wma_main.c
@@ -3600,11 +3600,13 @@ QDF_STATUS wma_open(struct wlan_objmgr_psoc *psoc,
 					   wma_peer_info_event_handler,
 					   WMA_RX_SERIALIZER_CTX);
 
+#ifdef WLAN_POWER_DEBUG
 	/* register for Chip Power stats event */
 	wmi_unified_register_event_handler(wma_handle->wmi_handle,
 				wmi_pdev_chip_power_stats_event_id,
 				wma_unified_power_debug_stats_event_handler,
 				WMA_RX_SERIALIZER_CTX);
+#endif
 #ifdef WLAN_FEATURE_BEACON_RECEPTION_STATS
 	/* register for beacon stats event */
 	wmi_unified_register_event_handler(wma_handle->wmi_handle,
@@ -7900,6 +7902,7 @@ static inline QDF_STATUS wma_send_wow_pulse_cmd(tp_wma_handle wma_handle,
  *
  * Return: QDF_STATUS
  */
+#ifdef WLAN_POWER_DEBUG
 static QDF_STATUS wma_process_power_debug_stats_req(tp_wma_handle wma_handle)
 {
 	wmi_pdev_get_chip_power_stats_cmd_fixed_param *cmd;
@@ -7937,6 +7940,12 @@ static QDF_STATUS wma_process_power_debug_stats_req(tp_wma_handle wma_handle)
 	}
 	return QDF_STATUS_SUCCESS;
 }
+#else
+static QDF_STATUS wma_process_power_debug_stats_req(tp_wma_handle wma_handle)
+{
+	return QDF_STATUS_SUCCESS;
+}
+#endif
 #ifdef WLAN_FEATURE_BEACON_RECEPTION_STATS
 static QDF_STATUS wma_process_beacon_debug_stats_req(tp_wma_handle wma_handle,
 						     uint32_t *vdev_id)
