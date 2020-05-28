f5023a18b81b7773f4d4596df29fd5cbe252c084
Srikanth Marepalli <marepall@codeaurora.org>
2020-05-29 03:38:04 +0530

    qcacld-3.0: Fix mem leak while deleting pmksa
    Due to commit : I95881db229d5193cbdc22c5f30e1375b3892fbd4,
    while processing set_pmk command driver checks pmk_len to delete
    pmk. In case if new PMK gets added with 0 length, pmk entry with
    pmk_len = 0 will never be deleted.
    It is only added without freeing the existing entry. Fix is to
    modify del pmk logic to avoid mem leak.
    
    Change-Id: I65c8a15e376826670bf6ca49b500bb25aa682a22
    CRs-Fixed: 2701754

--- core/sme/src/csr/csr_inside_api.h
+++ core/sme/src/csr/csr_inside_api.h
@@ -840,7 +840,7 @@ QDF_STATUS csr_roam_del_pmkid_from_cache(struct mac_context *mac,
 					 bool flush_cache);
 
 void csr_roam_del_pmk_cache_entry(struct csr_roam_session *session,
-				  tPmkidCacheInfo *cached_pmksa);
+				  tPmkidCacheInfo *cached_pmksa, u32 del_idx);
 
 #if defined(WLAN_SAE_SINGLE_PMK) && defined(WLAN_FEATURE_ROAM_OFFLOAD)
 /**
