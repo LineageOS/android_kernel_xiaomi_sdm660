6717c1083b70ebaa970cfb9341c527d77e181a10
Edwin Moquete <edwinmmoquete@gmail.com>
2020-10-28 13:13:11 +0000

    input: touchscreen: nvt_touch_*: Update wakeup source for 4.19
    Change-Id: Iee2d4824b8bf2595f1fd870401664f7444920f1d

--- drivers/input/touchscreen/nvt_touch_d2s/nt36xxx.c
+++ drivers/input/touchscreen/nvt_touch_d2s/nt36xxx.c
@@ -804,8 +804,6 @@ static int32_t nvt_flash_proc_init(void)
 /* function page definition */
 #define FUNCPAGE_GESTURE         1
 
-static struct wakeup_source gesture_wakeup;
-
 /*******************************************************
 Description:
 	Novatek touchscreen wake up gesture key report function.
@@ -1164,7 +1162,7 @@ static irqreturn_t nvt_ts_irq_handler(int32_t irq, void *dev_id)
 
 #if WAKEUP_GESTURE
 	if (bTouchIsAwake == 0) {
-		__pm_wakeup_event(&gesture_wakeup, msecs_to_jiffies(5000));
+		__pm_wakeup_event(ts->gesture_wakeup, msecs_to_jiffies(5000));
 	}
 #endif
 
@@ -1392,7 +1390,7 @@ static int32_t nvt_ts_probe(struct i2c_client *client, const struct i2c_device_i
 	for (retry = 0; retry < (sizeof(gesture_key_array) / sizeof(gesture_key_array[0])); retry++) {
 		input_set_capability(ts->input_dev, EV_KEY, gesture_key_array[retry]);
 	}
-	wakeup_source_init(&gesture_wakeup, "poll-wake-lock");
+	ts->gesture_wakeup = wakeup_source_register(NULL, "poll-wake-lock");
 #ifdef CONFIG_TOUCHSCREEN_COMMON
 	ret = tp_common_set_double_tap_ops(&double_tap_ops);
 	if (ret < 0) {
