d53a5534ebdddf5989da36af90309180ef814bba
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-11-23 15:56:55 +0100

    sound: msm: Add tfa9894 support
    Change-Id: I24f596dd61c763979e7435d0c992c7b4f4fe771d

--- sound/soc/msm/sdm660-internal.c
+++ sound/soc/msm/sdm660-internal.c
@@ -2856,8 +2856,13 @@ static struct snd_soc_dai_link msm_mi2s_be_dai_links[] = {
 		.stream_name = "Secondary MI2S Playback",
 		.cpu_dai_name = "msm-dai-q6-mi2s.1",
 		.platform_name = "msm-pcm-routing",
+#ifdef CONFIG_SND_SOC_TFA9894
+		.codec_name = "tfa98xx.2-0034",
+		.codec_dai_name = "tfa98xx-aif-2-34",
+#else
 		.codec_name = "msm-stub-codec.1",
 		.codec_dai_name = "msm-stub-rx",
+#endif
 		.no_pcm = 1,
 		.dpcm_playback = 1,
 		.be_id = MSM_BACKEND_DAI_SECONDARY_MI2S_RX,
