3b3a439fe57669a65ffad7a8590a3affa1b4fcd3
Vulupala Shashank Reddy <vulupa@codeaurora.org>
2021-02-22 16:23:38 +0530

    qcacmn: Add CDP intf to flush fragments for a particular peer
    Add CDP interface support for flushing fragments for a
    particular peer.
    
    Change-Id: Ic59843408037b9c858599be13727422c2e3031cd
    CRs-Fixed: 2875946

--- dp/inc/cdp_txrx_ops.h
+++ dp/inc/cdp_txrx_ops.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2020 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2017-2021 The Linux Foundation. All rights reserved.
  *
  *
  * Permission to use, copy, modify, and/or distribute this software for
@@ -1355,6 +1355,8 @@ struct cdp_peer_ops {
 	void (*update_last_real_peer)(struct cdp_pdev *pdev, void *vdev,
 			uint8_t *peer_id, bool restore_last_peer);
 	void (*peer_detach_force_delete)(void *peer);
+	void (*peer_flush_frags)(struct cdp_pdev *pdev,
+				 uint8_t vdev_id, uint8_t *peer_mac);
 };
 
 /**
